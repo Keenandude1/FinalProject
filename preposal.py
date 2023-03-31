4a73b37fc756409db6c7975737682a37feee81a0
Keenandude1 <129561599+Keenandude1@users.noreply.github.com>
2023-03-31 19:36:17 -0400

    Add files via upload

---
+++ preposal.py
@@ -0,0 +1,20 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+# For our final project we are debating between making a snake game, a tron
+# game, or a financial advice software to help people with investing based on
+# their age, income, and amount wanting to invest
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+I plan to collaborate with Evan Burleson.
+
+## Do you have any questions of your own?
+
+# I know just a snake game by itself would not be complex enough but would us
+# coding a tron game by itself be complex enough because we would have to make
+# the turtles listen and that they would die when running into previously drawn
+# lines and the boundaries around the game would have to be increased or turtle
+# size decreased so that the game could be bigger and last longer.
+
