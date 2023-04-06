81d4ee1162d26140e495508253f14259c79181fe
evanburleson1 <128819527+evanburleson1@users.noreply.github.com>
2023-04-06 15:12:33 -0400

    Add files via upload

---
+++ FinalPpP.py
@@ -0,0 +1,40 @@
+# Proposal
+
+## What will (likely) be the title of your project?
+
+TODO TRON the video game 
+
+## In just a sentence or two, summarize your project. (E.g., "A website that lets you buy and sell stocks.")
+
+TODO Two player game that allows for user control to move a bike around the map and leave a trail of their past movements behind. 
+
+## In a paragraph or more, detail your project. What will your software do? What features will it have? How will it be executed?
+
+TODO Our project with be guided by user inputs. Establishes boundries around the map and from the trails players leave behind as they move. It will be executed using the pygame interface for more user friendly experience.
+
+## If planning to combine 1051's final project with another course's final project, with which other course? And which aspect(s) of your proposed project would relate to 1051, and which aspect(s) would relate to the other course?
+
+TODO, if applicable Not applicable
+
+## If planning to collaborate with 1 or 2 classmates for the final project, list their names, email addresses, and the names of their assigned TAs below.
+
+TODO, if applicable Roman Jackson, romankj@temple.edu, Rahad
+Evan Burleson, tun31930@temple.edu, Rahad
+
+## In the world of software, most everything takes longer to implement than you expect. And so it's not uncommon to accomplish less in a fixed amount of time than you hope.
+
+### In a sentence (or list of features), define a GOOD outcome for your final project. I.e., what WILL you accomplish no matter what?
+
+TODO A working TRON game that breaks when lines are crossed and properly resets the map.
+
+### In a sentence (or list of features), define a BETTER outcome for your final project. I.e., what do you THINK you can accomplish before the final project's deadline?
+
+TODO Implement a scoring system between the two players and special effects for the winner.
+
+### In a sentence (or list of features), define a BEST outcome for your final project. I.e., what do you HOPE to accomplish before the final project's deadline?
+
+TODO Create a website that runs the TRON game
+
+## In a paragraph or more, outline your next steps. What new skills will you need to acquire? What topics will you need to research? If working with one of two classmates, who will do what?
+
+TODO We need to research how to involve user control and reset the map entirely in pygame and how to use the pygame interface in general as we are mnot familiar with it.
